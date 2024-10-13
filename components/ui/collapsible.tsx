8f541a18480f8055507fe83c9ce6d19307a5fa6d
Paritosh Patil <paritosh.patil18@vit.edu>
2024-10-14 00:16:46 +0530

    added missing ui components

---
+++ components/ui/collapsible.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+import {cn} from "@/lib/utils";
+import React from "react";
+
+const Collapsible = CollapsiblePrimitive.Root
+
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
+
+const CollapsibleContent = React.forwardRef<
+    React.ElementRef<typeof CollapsiblePrimitive.CollapsibleContent>,
+    React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.CollapsibleContent>
+>(({ className, ...props }, ref) => (
+    <CollapsiblePrimitive.CollapsibleContent
+        ref={ref}
+        className={cn('overflow-hidden data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up', className)}
+        {...props}
+    />
+));
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }

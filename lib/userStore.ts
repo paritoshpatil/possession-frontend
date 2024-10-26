e94bd3704295ffca4f79c6e66e0b5c2c914f8026
Paritosh Patil <paritosh.patil18@vit.edu>
2024-10-26 23:39:30 +0530

    fixed all errors for prod build, improved locations page

--- lib/userStore.ts
+++ lib/userStore.ts
@@ -1,7 +1,26 @@
 import {create} from "zustand";
 
+
+interface User {
+    id:	string
+    aud:	string
+    role:	string
+    email:	string
+    email_confirmed_at:	string
+    phone:	string
+    phone_confirmed_at:	string
+    confirmed_at:	string
+    last_sign_in_at:	string
+    app_metadata:	object
+    user_metadata:	object
+    created_at:	string
+    updated_at:	string
+    is_anonymous:	boolean
+}
+
+
 interface UserStore {
-    user: Object | null
+    user: User | null
     setUser: (user: any) => void
     isLoggedIn: boolean
 }

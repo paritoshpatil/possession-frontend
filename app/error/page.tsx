3749b83a86ec4e477019bd35228e028d5fbfb02e
Paritosh Patil <paritoshpatil54321@gmail.com>
2025-02-06 16:26:46 +0530

    Minor changes

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,3 +1,3 @@
 export default  function ErrorPage() {
-    return <h1>There was an error processing your request</h1>
+    return <h1>There was an error processing your request, please try again ... </h1>
 }
